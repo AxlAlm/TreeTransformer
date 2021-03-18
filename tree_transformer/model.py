8ac12c96c3f0a2e25a638ce21d87ce2155dcc10a
Axel <axelalmquist@gmail.com>
2021-03-18 09:40:05 +0100

    init2

---
+++ tree_transformer/model.py
@@ -0,0 +1,26 @@
+
+
+import torch
+from torch import nn
+
+
+class TreeTransformer(nn.Module):
+
+
+    def __init__(self):
+        pass
+
+
+
+    def decoder(self):
+        pass
+
+
+
+    def encoder(self):
+        pass
+
+
+
+    def forward(self):
+        pass
